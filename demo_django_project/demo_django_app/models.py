827b8683f0b2d0776ce67500e8a02352c16b7e1d
Jake Kara <jake@jakekara.com>
2023-11-24 10:19:04 -0500

    add choice example to demo

--- demo_django_project/demo_django_app/models.py
+++ demo_django_project/demo_django_app/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class Person(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
+    shoeStyle = models.CharField(
+        max_length=30, choices=[("LO", "LOAFER"), ("BO", "BOOT"), ("DR", "DRESS")]
+    )
